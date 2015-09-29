d243c37c025d7577a9385289a1940b27dc879ecd
Liz Stevenson <lizstevenson4@gmail.com>
2015-09-29 14:26:05 +0100

    First draft of jason app

---
+++ javascript/js.js
@@ -0,0 +1,24 @@
+
+<!---------Smooth scroll js -------->
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+<!-------this calls collapse on the nav bar toggle element when clicked --------->
+
+$(document).ready(function () {
+$(".navbar-nav li a").click(function(event) {
+$(".navbar-collapse").collapse('hide');
+});
+});
